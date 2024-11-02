4fb29bf6de7c9f478c84bd857dcedb5536d3bfaa
LizzieK549 <klizzy630@gmail.com>
2024-11-01 19:06:19 -0500

    Week 9 project
    Lizzie Kerr, Project 9, 11/1/2024

--- program_3.py
+++ program_3.py
@@ -9,10 +9,21 @@
 # It should handle any ValueError exceptions that are raised when the items that are read from the file 
 # are converted to a number.
 def sum_numbers_from_file():
-    ######################
-    # Add your code here #
-    ######################
+    total = 0
+
+    try:
+        with open('numbers.txt', 'r') as file:
+            for line in file:
+                try:
+                    number = int(line.strip())
+                    total += number
+                except ValueError:
+                    print(f"ValueError converting: '{line.strip()}'")
+    except IOError:
+        print("Couldn't find 'numbers.txt', make sure it's actually there")
+
     print('In the sum_numbers_from_file function')
+    print('The sum of the numbers is: ', total)
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
